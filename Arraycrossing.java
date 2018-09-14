6b59b824c3f4aa666e1783e4a4b690b19eb4017c
Leon-Scott-Kennedy <43233978+Leon-Scott-Kennedy@users.noreply.github.com>
2018-09-14 21:18:00 +0800

    Add files via upload

---
+++ Arraycrossing.java
@@ -0,0 +1,11 @@
+public class Arraycrossing{
+     public static void main(String[] argv){
+        int [][] array = {{1,2,3},{1,4}};
+        System.out.println(array[1][2]);  
+  }
+ 
+}
+
+
+//Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 2
+        at Arraycrossing.main(Arraycrossing.java:4)
